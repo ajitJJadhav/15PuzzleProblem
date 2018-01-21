47f85da6ea13acb8bda2e0798173029dacce29f7
Ajit Jadhav <ajitopensource@gmail.com>
2018-01-21 16:44:37 +0530

    completed heap structure

---
+++ heap.c
@@ -0,0 +1,164 @@
+#include<stdio.h>
+#include <string.h>
+#include <math.h>
+#include<limits.h>
+#include<stdlib.h>
+
+
+struct PuzzleState
+{
+	char puzzle[40];
+	int depth;
+	int heuristicValue;
+  struct PuzzleState* parent;
+};
+
+
+struct PuzzleState* heap[1000000];
+int heapSize;
+
+/*Initialize Heap*/
+
+void Init() {
+    heapSize = 0;
+    heap[0] = (struct PuzzleState*)malloc(sizeof(struct PuzzleState));
+    heap[0]->heuristicValue = heap[0]->depth = 0;
+}
+
+/*Insert an element into the heap */
+
+struct PuzzleState* createNode(char puzzle[],int heuristicValue,int depth)
+{
+  struct PuzzleState* temp;
+  temp = (struct PuzzleState*)malloc(sizeof(struct PuzzleState));
+
+
+  strcpy(temp->puzzle,puzzle);
+  temp->heuristicValue = heuristicValue;
+  temp->depth = depth;
+  temp->parent = NULL;
+
+  return temp;
+}
+
+void printNode(struct PuzzleState *temp)
+{
+  printf("\n%d\n%s\n ", temp->heuristicValue, temp->puzzle);
+}
+
+void Insert(struct PuzzleState *element) {
+  heapSize++;
+
+  heap[heapSize] = element; /*Insert in the last place*/
+  /*Adjust its position*/
+
+  int now = heapSize;
+
+  while ( heap[now / 2]->heuristicValue + heap[now / 2]->depth > element->heuristicValue + element->depth ) {
+
+      heap[now] = heap[now / 2];
+
+      now /= 2;
+
+  }
+
+  heap[now] = element;
+
+}
+
+struct PuzzleState* DeleteMin() {
+
+    /* heap[1] is the minimum element. So we remove heap[1]. Size of the heap is decreased.
+
+     Now heap[1] has to be filled. We put the last element in its place and see if it fits.
+
+     If it does not fit, take minimum element among both its children and replaces parent with it.
+
+     Again See if the last element fits in that place.*/
+
+    int child, now;
+    struct PuzzleState *minElement,*lastElement;
+
+    minElement = heap[1];
+
+    lastElement = heap[heapSize--];
+
+    /* now refers to the index at which we are now */
+
+    for (now = 1; now * 2 <= heapSize; now = child) {
+
+        /* child is the index of the element which is minimum among both the children */
+
+        /* Indexes of children are i*2 and i*2 + 1*/
+
+        child = now * 2;
+
+        /*child!=heapSize beacuse heap[heapSize+1] does not exist, which means it has only one
+
+         child */
+
+        if (child != heapSize && heap[child + 1]->heuristicValue + heap[child + 1]->depth < heap[child]->heuristicValue + heap[child]->depth) {
+
+            child++;
+
+        }
+
+        /* To check if the last element fits ot not it suffices to check if the last element
+
+         is less than the minimum element among both the children*/
+
+        if (lastElement->heuristicValue + lastElement->depth > heap[child]->heuristicValue + heap[child]->depth ) {
+
+            heap[now] = heap[child];
+
+        } else /* It fits there */
+
+        {
+
+            break;
+
+        }
+
+    }
+
+    heap[now] = lastElement;
+
+    return minElement;
+
+}
+
+
+
+int main() {
+
+    int number_of_elements;
+
+    printf("Program to demonstrate Heap:\nEnter the number of elements: ");
+
+    scanf("%d", &number_of_elements);
+
+    int iter;
+    struct PuzzleState* temp;
+
+    Init();
+
+
+    for (iter = 0; iter < number_of_elements; iter++) {
+        temp = (struct PuzzleState*)malloc(sizeof(struct PuzzleState));
+
+        scanf("%s %d", temp->puzzle, &temp->heuristicValue);
+        temp->depth = 0;
+        Insert(temp);
+
+    }
+
+    for (iter = 0; iter < number_of_elements; iter++) {
+        temp = DeleteMin();
+        printNode(temp);
+    }
+
+    printf("\n");
+
+    return 0;
+
+}
--- puzzle.c
+++ puzzle.c
@@ -3,26 +3,142 @@
 #include <string.h>
 #include <math.h>
 
+#include <iostream>
+using namespace std;
+
 struct PuzzleState
 {
 	char puzzle[40];
 	int depth;
-	int heuristicValue;	
+	int heuristicValue;
+	struct PuzzleState* parent;
 };
 
+struct PuzzleState* heap[1000000];
+int heapSize;
+
+/*Initialize Heap*/
+
+void Init() {
+    heapSize = 0;
+    heap[0] = (struct PuzzleState*)malloc(sizeof(struct PuzzleState));
+    heap[0]->heuristicValue = heap[0]->depth = 0;
+}
+
+
+struct PuzzleState* createNode(char puzzle[],int heuristicValue,int depth)
+{
+  struct PuzzleState* temp;
+  temp = (struct PuzzleState*)malloc(sizeof(struct PuzzleState));
+
+
+  strcpy(temp->puzzle,puzzle);
+  temp->heuristicValue = heuristicValue;
+  temp->depth = depth;
+  temp->parent = NULL;
+
+  return temp;
+}
+
+void printNode(struct PuzzleState *temp)
+{
+  printf("\n%d\n%s\n ", temp->heuristicValue, temp->puzzle);
+}
+
+/*Insert an element into the heap */
+void Insert(struct PuzzleState *element) {
+  heapSize++;
+
+  heap[heapSize] = element; /*Insert in the last place*/
+
+	/*Adjust its position*/
+  int now = heapSize;
+
+  while ( heap[now / 2]->heuristicValue + heap[now / 2]->depth > element->heuristicValue + element->depth ) {
+
+      heap[now] = heap[now / 2];
+
+      now /= 2;
+
+  }
+
+  heap[now] = element;
+
+}
+
+struct PuzzleState* DeleteMin() {
+
+    /* heap[1] is the minimum element. So we remove heap[1]. Size of the heap is decreased.
+
+     Now heap[1] has to be filled. We put the last element in its place and see if it fits.
+
+     If it does not fit, take minimum element among both its children and replaces parent with it.
+
+     Again See if the last element fits in that place.*/
+
+    int child, now;
+    struct PuzzleState *minElement,*lastElement;
+
+    minElement = heap[1];
+
+    lastElement = heap[heapSize--];
+
+    /* now refers to the index at which we are now */
+
+    for (now = 1; now * 2 <= heapSize; now = child) {
+
+        /* child is the index of the element which is minimum among both the children */
+
+        /* Indexes of children are i*2 and i*2 + 1*/
+
+        child = now * 2;
+
+        /*child!=heapSize beacuse heap[heapSize+1] does not exist, which means it has only one
+
+         child */
+
+        if (child != heapSize && heap[child + 1]->heuristicValue + heap[child + 1]->depth < heap[child]->heuristicValue + heap[child]->depth) {
+
+            child++;
+
+        }
+
+        /* To check if the last element fits ot not it suffices to check if the last element
+
+         is less than the minimum element among both the children*/
+
+        if (lastElement->heuristicValue + lastElement->depth > heap[child]->heuristicValue + heap[child]->depth ) {
+
+            heap[now] = heap[child];
+
+        } else /* It fits there */
+
+        {
+
+            break;
+
+        }
+
+    }
+
+    heap[now] = lastElement;
+
+    return minElement;
+
+}
 
 
 
 int**  loadDataFromStringToMatrix(struct PuzzleState x)
 {
-	
+
 
 	int **matrix = (int**)malloc(4 * sizeof(int*));
 	for(int k = 0; k < 4 ;k++)
 	{
 		matrix[k] = (int*)malloc(4 * sizeof(int));
-	} 
-	
+	}
+
 
 
 	for(int i = 0; i < 4; i++)
@@ -52,13 +168,13 @@ int heuristic_1(struct PuzzleState x)
 {
 	int **matrix;
 	int count = 0;
-	
+
 	matrix = loadDataFromStringToMatrix(x);
 	for(int i = 0; i < 4 ; i++)
 	{
 		for(int j = 0; j < 4; j++)
 		{
-			if(matrix[i][j] != 4*i + j  && a != 15) 
+			if(matrix[i][j] != 4*i + j  && matrix[i][j] != 15)
 			{
 				count += 1 ;
 			}
@@ -105,7 +221,14 @@ void displayMatrix(int **matrix)
 	{
 		for (int j = 0; j < 4; ++j)
 		{
-			printf("%d ", matrix[i][j] );
+			if(matrix[i][j] != 15)
+			{
+				printf("%d ", matrix[i][j] + 1 );
+			}
+			else
+			{
+				printf("B ");
+			}
 		}
 		printf("\n");
 	}
@@ -119,6 +242,9 @@ void displayMatrix(int **matrix)
 int main()
 {
 	struct PuzzleState x;
-	strcpy(x.puzzle,"0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15");
+	strcpy(x.puzzle,"4,1,2,3,0,5,6,7,8,9,10,11,12,13,14,15");
+
+	cout << heuristic_1(x) << endl << heuristic_2(x) << endl;
+
 	return 0;
 }
